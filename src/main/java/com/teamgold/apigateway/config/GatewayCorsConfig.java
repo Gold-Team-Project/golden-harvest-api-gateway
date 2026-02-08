e296bd1653bfb88151702992d1ebf1bcdebc6cbe
405supply <405supplychoi@gmail.com>
2026-02-08 21:48:32 +0900

    fix: cloudfront cors

--- src/main/java/com/teamgold/apigateway/config/GatewayCorsConfig.java
+++ src/main/java/com/teamgold/apigateway/config/GatewayCorsConfig.java
@@ -14,7 +14,11 @@ public class GatewayCorsConfig {
     @Bean
     public CorsWebFilter corsWebFilter() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://d6y5hv3d5nlk0.cloudfront.net",  // CloudFront 기본 주소
+                "https://gharvest.shop",                 // 실제 연결할 도메인 (HTTPS)
+                "http://gharvest.shop"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setExposedHeaders(List.of("X-GW-REJECT"));
